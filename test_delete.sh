129e5737ade3508c12ed53ba7185067c0a13689b
Antonina Klimchuk <hggty1804@gmail.com>
2025-03-29 00:37:06 +0300

    added tests for insert, replace position, delete

---
+++ test_delete.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+size=1048576
+
+dd if=/dev/random of=test.bin bs=1 count=$size
+
+for i in {1..20}; do
+    echo "************************************************"
+    echo "Test number $i"
+    echo "************************************************"
+
+    idx=$((RANDOM % 200))
+
+    head -c "$idx" test.bin > test2.bin #idx включительно
+    tail -c "+$((idx + 2))" test.bin >> test2.bin #idx + 1 не включен
+
+    ./main test.bin > manifest
+    ./main test2.bin > manifest2
+
+    diff manifest manifest2
+done
